edf21034d54dccba196b9452dbd7d64c30ffb80e
kwanghan-bae <kwanghan.bae@gmail.com>
2025-11-07 17:32:56 +0900

    Refactor: Feign을 WebClient로 변경

--- app/api/src/main/kotlin/com/wallet/clover/api/CloverApiApplication.kt
+++ app/api/src/main/kotlin/com/wallet/clover/api/CloverApiApplication.kt
@@ -2,10 +2,8 @@ package com.wallet.clover.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication(scanBasePackages = ["com.wallet.clover"])
-@EnableFeignClients(basePackages = ["com.wallet.clover"])
 class InternalApiApplication
 
 fun main(args: Array<String>) {
--- domain/src/main/kotlin/com/wallet/clover/domain/YesNo.kt
+++ domain/src/main/kotlin/com/wallet/clover/domain/YesNo.kt
@@ -2,9 +2,5 @@ package com.wallet.clover.domain
 
 enum class YesNo {
     Y,
-    N;
-
-    fun isYes() = this == Y
-
-    fun isNo() = this == N
+    N,
 }
