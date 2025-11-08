4cdcad63dd0b6a17e2ff2537cf089b66f5934ba0
kwanghan-bae <kwanghan.bae@gmail.com>
2025-11-08 22:01:35 +0900

    Refactor: 미사용 RecommendSpec.kt 파일 제거 및 CloverApiApplication 수정

--- app/api/src/main/kotlin/com/wallet/clover/api/CloverApiApplication.kt
+++ app/api/src/main/kotlin/com/wallet/clover/api/CloverApiApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class CloverApiApplication
 
 fun main(args: Array<String>) {
-    runApplication<InternalApiApplication>(*args)
+    runApplication<CloverApiApplication>(*args)
 }
--- app/api/src/main/kotlin/com/wallet/clover/api/endpoint/RecommendSpec.kt
+++
@@ -1,7 +0,0 @@
-package com.wallet.clover.api.endpoint
-
-import com.wallet.clover.domain.recommendation.Recommendation
-
-interface RecommendSpec {
-    fun get(): Recommendation
-}
